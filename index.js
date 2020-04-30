397feae0370daecbe1d91d17a3ae58824a0577f5
FelixMo42 <felix@tacocat.com>
2020-04-29 21:09:12 -0700

    add string matching to the parser

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require("fs")
-const { Map } = require("immutable")
+const immutable = require("immutable")
 const Language = require("./lib/Language")
 
 const UnWrap = ([term]) => term
@@ -10,11 +10,12 @@ const Function = ([ params, value ]) => ({ type: "function", params, value })
 const FunctionCall = ([ func, arguments ]) => ({ type: "functionCall", func, arguments })
 const If = ([ condtion, then, el ]) => ({ type: "if", condtion, then, el })
 const Number = ([ number ]) => ({ type: "number", value: parseInt(number)})
-const Refrence = ([ name ]) => ({ type: "refrence", name })
+const Variable = ([ name ]) => ({ type: "variable", name })
+const Decleration = ([ key, value ]) => ({ type: "decleration" })
 
-const Scope = (parent=Map()) => parent
-const Variable = (scope, key, type, value) => {
-}
+// const Programme = (data) => ({ functions: new Map(), baseScope: immutable.Map(data) })
+// const Scope = (parent=immutable.Map()) => parent
+// const Variable = (scope, key, type, value) => scope.set(({key, type, value}))
 
 //
 
@@ -23,32 +24,34 @@ let Parse = Language(({Word, KeyWord, Rule, Loop, Case}) => ({
     IF : KeyWord("if"),
 
     // puncutation
-    SPEC : KeyWord("specifier", /^:/),
-    OPEN : KeyWord("open parentheses", /^\(/),
-    CLOSE : KeyWord("close parentheses", /^\)/),
+    PUNCS : KeyWord("specifier", /^[\:\[\]\(\)\{\}]/),
 
     // words in the grammar
     number : Word("number", /^[0-9]*/),
     name : Word("name", /^[a-zA-Z\*\-\+\/\^\=]*/),
 
     // parsing rules
-    value : Rule("value", ({ IF, OPEN, CLOSE, number, name, value, paramaters, arguments }) => [
-        Case([ name ], Refrence),
+    value : Rule("value", ({ number, name, value, paramaters, arguments }) => [
+        Case([ name ], Variable),
         Case([ number ], Number),
 
-        Case([ OPEN, OPEN, paramaters, CLOSE, value, CLOSE ], Function),
+        Case([ "(", "[", paramaters, "]", value, ")" ], Function),
 
-        Case([ OPEN, value, arguments, CLOSE ], FunctionCall),
+        Case([ "(", value, arguments, ")" ], FunctionCall),
 
-        Case([ IF, value, value, value ], If)
+        Case([ "if", value, value, value ], If)
     ]),
 
     arguments : Rule("arguments", ({ value }) => [
         Case([ Loop( value ) ])
     ]),
 
-    paramaters : Rule("params", ({ SPEC, name, type }) => [
-        Case([ Loop(name, SPEC, type) ])
+    paramaters : Rule("paramaters", ({ paramater }) => [
+        Case([ Loop( paramater ) ])
+    ]),
+
+    paramater : Rule("paramaters", ({ name, type }) => [
+        Case([ name, ":", type ])
     ]),
 
     type : Rule("type", ({ name }) => [
@@ -62,9 +65,32 @@ let Parse = Language(({Word, KeyWord, Rule, Loop, Case}) => ({
 //
 
 const ast = Parse( fs.readFileSync("main.ust").toString() )
+// console.log( JSON.stringify(ast, null, "   ") )
 
-const root = Scope()
+const Run = (node, scope=immutable.Map()) => {
+    if ( node.type == "number" ) return node.value
 
+    if ( node.type == "function" ) return node
 
+    if ( node.type == "variable" ) return scope.get(node.name)
 
-console.log( JSON.stringify(ast, null, "   ") )
+    if ( node.type == "functionCall" ) {
+        let func = Run(node.func)
+
+        console.log(func)
+
+        // for (let i = 0; i < node.arguments.length; i++) {
+        //     scope = scope.set( node.paramaters[i],  )
+        // }
+
+        // return Run(value, scope)
+    }
+
+    // if ( node.type == "" ) 
+}
+
+console.log(ast)
+
+Run(ast, immutable.Map({
+    "+": Function([], )
+}))
